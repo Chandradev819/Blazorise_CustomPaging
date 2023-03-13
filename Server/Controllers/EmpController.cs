21622730252b6453c5de11cd865a32a26e509a67
Chandradev Prasad <chandradev.prasad@channelmechanics.com>
2023-03-13 17:48:16 +0530

    Add project files.

---
+++ Server/Controllers/EmpController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazorise.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        public EmpController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
---
+++ Shared/Emp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazorise.Shared
+{
+    public class Emp
+    {
+        public int id { get; set; }
+        public string imageUrl { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string contactNumber { get; set; }
+        public int age { get; set; }
+        public string dob { get; set; }
+        public float salary { get; set; }
+        public string address { get; set; }
+    }
+}
